8c6c1dc6beb4b2d3884298619d24f226647e0577
Dillies Benoît <benoit.dillies@ynov.com>
2018-06-04 20:49:29 +0200

    commit  1

---
+++ app.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client
+
+bot.login('NDUzMjM2NjEwMDI1MTI3OTUw.DfcDtg.TMRAcMvCROc3ow6S7ZEt9IXKmZk')
+
+bot.on('message', function(message){
+    if (message.content === '!ping') {
+        message.reply('ntm fdp')
+    }
+})
